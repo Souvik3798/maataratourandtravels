99562068903c5243ed719c4eb080e6e69217b332
Souvik3798 <souvikbarua693@gmail.com>
2024-07-17 01:50:28 +0530

    location

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -165,10 +165,10 @@
                                 <div class="intro_item_overlay"></div>
                                 <div class="intro_item_background" style="background-image:url({{asset('storage/'.$location->image_path)}})"></div>
                                 <div class="intro_item_content d-flex flex-column align-items-center justify-content-center">
-                                    <div class="intro_date">{{$location->Place}}</div>
+                                    <div class="intro_date">{{$location->Location}}</div>
                                     <div class="button intro_button"><div class="button_bcg"></div><a href={{url('location/'.$location->id)}}>see more<span></span><span></span><span></span></a></div>
                                     <div class="intro_center text-center">
-                                        <h1>{{$location->Location}}</h1>
+                                        <h1>{{$location->Place}}</h1>
                                         <div class="rating rating_4">
                                             <i class="fa fa-star"></i>
                                             <i class="fa fa-star"></i>
