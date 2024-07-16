453a7f450e8cca36b77cd24f920fa0b189be806e
Souvik3798 <souvikbarua693@gmail.com>
2024-07-17 00:22:51 +0530

    margin bottom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -158,7 +158,7 @@
                         $locations = \App\Models\TourLocation::all();
                     @endphp
                     @foreach ($locations as $location)
-                        <div class="col-lg-4 intro_col" style="padding-bottom: 10px">
+                        <div class="col-lg-4 intro_col" style="margin-bottom: 10px">
                             <div class="intro_item">
                                 <div class="intro_item_overlay"></div>
                                 <div class="intro_item_background" style="background-image:url({{asset('storage/'.$location->image_path)}})"></div>
