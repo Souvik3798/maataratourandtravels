1053c7ed4a5e08e358a5e3ed9baa5f27f77e5022
Souvik3798 <souvikbarua693@gmail.com>
2024-07-17 14:29:36 +0530

    keywords

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Best Travels Agent in Andaman, Holiday Tour Package </title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Travelix Project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{{asset('styles/bootstrap4/bootstrap.min.css')}}">
-<link href={{asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}} rel="stylesheet" type="text/css">
-<link href="{{asset('plugins/colorbox/colorbox.css" rel="stylesheet')}}" type="text/css">
-<link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/animate.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('styles/main_styles.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('styles/responsive.css')}}>
-<link rel="shortcut icon" type="image/x-icon" href={{asset('images/ABSsmall.png')}}>
+    <title>Best Travel Agent in Andaman - Holiday Tour Packages | Andaman Blue Ocean</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Explore the best holiday tour packages and bespoke travel experiences in Andaman and Nicobar Islands with Andaman Blue Ocean. Your trusted travel partner since 2006.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="application-name" content="Andaman Blue Ocean">
+    <meta name="keywords" content="Andaman travel, Andaman tours, holiday packages, Andaman holiday tours, Port Blair travel agent, Nicobar travel, Andaman and Nicobar Islands, Andaman best travel agency">
+    <link rel="stylesheet" type="text/css" href="{{asset('styles/bootstrap4/bootstrap.min.css')}}">
+    <link href={{asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}} rel="stylesheet" type="text/css">
+    <link href="{{asset('plugins/colorbox/colorbox.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}>
+    <link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}>
+    <link rel="stylesheet" type="text/css" href={{asset('plugins/OwlCarousel2-2.2.1/animate.css')}}>
+    <link rel="stylesheet" type="text/css" href={{asset('styles/main_styles.css')}}>
+    <link rel="stylesheet" type="text/css" href={{asset('styles/responsive.css')}}>
+    <link rel="shortcut icon" type="image/x-icon" href={{asset('images/ABSsmall.png')}}>
 
 <style>
     .logo_container .logo img {
