31cd3eb6693b2ef234eb3296a2370bdfb6792995
Souvik3798 <souvikbarua693@gmail.com>
2024-07-23 21:08:16 +0530

    kjbj

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -708,8 +708,9 @@
     </div>
 
     <!-- Chat Button -->
-    <div class="chat-button" id="chatButton" style="width: 30px">
-        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/headphone-4035933-3342612.png?f=webp" alt="Chat Icon">
+    <div class="chat-button" id="chatButton" style="width: 40px; height: 40px">
+        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/headphone-4035933-3342612.png?f=webp" alt="Chat Icon"
+            style="width: 40px; height: 40px">
     </div>
 
     <!-- Chat Modal -->
