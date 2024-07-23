8f7faa3999825cc315be1291e87e5979ce8c37c3
Souvik3798 <souvikbarua693@gmail.com>
2024-07-23 22:56:35 +0530

    logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -131,6 +131,18 @@
             /* Adjust as needed to avoid overlap with WhatsApp button */
             right: 20px;
         }
+
+        .main_nav .main_nav_list {
+            margin-left: 0;
+        }
+
+        .main_nav .main_nav_item {
+            margin-right: 1rem;
+        }
+
+        .logo_container {
+            margin-right: 1rem;
+        }
     </style>
 </head>
 
@@ -175,10 +187,10 @@
                         <div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
                             <div class="main_nav_container ml-auto">
                                 <ul class="main_nav_list">
-                                    <li class="main_nav_item" style="text-align: right; margin-left: auto;"><a
-                                            href="{{ url('/') }}">home</a></li>
-                                    <li class="main_nav_item" style="text-align: right;"><a
-                                            href="{{ url('about/') }}">about us</a></li>
+                                    <li class="main_nav_item home-about-adjust"><a href="{{ url('/') }}">home</a>
+                                    </li>
+                                    <li class="main_nav_item home-about-adjust"><a href="{{ url('about/') }}">about
+                                            us</a></li>
                                 </ul>
                             </div>
                             <div class="logo_container">
@@ -197,24 +209,6 @@
                                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="17px"
                                     height="17px" viewBox="0 0 512 512" enable-background="new 0 0 512 512"
                                     xml:space="preserve">
-                                    <g>
-                                        <g>
-                                            <g>
-                                                {{-- <path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
-											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
-											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
-											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
-											c0,96.725-78.691,175.416-175.416,175.416C198.495,392.195,119.804,313.505,119.804,216.78z"/> --}}
-                                            </g>
-                                        </g>
-                                        <g>
-                                            <g>
-                                                {{-- <path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
-											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
-											C-2.019,484.77-2.019,497.865,6.057,505.942z"/> --}}
-                                            </g>
-                                        </g>
-                                    </g>
                                 </svg>
                             </div>
 
