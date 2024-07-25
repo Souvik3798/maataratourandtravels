961f533912d6413d6e53cb9a8c7c8093e511e0e0
Souvik3798 <souvikbarua693@gmail.com>
2024-07-25 21:45:29 +0530

    "Updated welcome.blade.php: added text shadow to two h1 elements and added a new h1 element."

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -453,8 +453,9 @@
                     <div class="home_slider_content text-center">
                         <div class="home_slider_content_inner" data-animation-in="flipInX"
                             data-animation-out="animate-out fadeOut">
-                            <h1> </h1>
-                            <h1>Andaman Blue Ocean Travels</h1>
+                            <h1 style="text-shadow: 2px 2px 5px rgba(0,0,0,0.3);">Welcome</h1>
+                            <h1 style="text-shadow: 2px 2px 5px rgba(0,0,0,0.3);">Andaman Blue Ocean Travels</h1>
+
                             <div class="button home_slider_button purple_button" style="border: 2px solid white">
                                 <div class="button_bcg">
                                 </div>
