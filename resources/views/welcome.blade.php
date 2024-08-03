b8044d0393bb4ef9627b2e56286d74f2831944e4
Souvik3798 <souvikbarua693@gmail.com>
2024-08-03 13:23:14 +0530

    "Change origin direction from 'top' to 'left' in reveal animation"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1522,7 +1522,7 @@
             duration: 1000, // Animation duration in milliseconds
             distance: '500px', // Distance to move during the reveal
             easing: 'ease-in', // Easing function
-            origin: 'top', // Direction the element will move from (bottom, top, left, right)
+            origin: 'left', // Direction the element will move from (bottom, top, left, right)
         });
     </script>
 
